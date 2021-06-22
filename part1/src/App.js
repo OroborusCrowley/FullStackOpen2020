f284917516ca41f3af90eb90c836801b2f7ae867
Carlos Francisco Paz Cuevas <cpaz@caduinmobiliaria.com>
2021-06-22 16:48:56 -0500

    1.2: course information, step2 An array was created with the elements of the body - Header - Content - Total with his attributes.
    And to resolve the excercise, each part of the content was dinamycally created and added to the main component of Content

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,45 +1,76 @@
 import React from 'react'
 
 const App = () => {
+  const elements = {
+    header: {
+      title: "Header",
+      course_name: "Half Stack application development"
+    },
+    parts:{
+      title:"Parts",
+      elements:
+      [
+        {
+          title:"Fundamentals of React",
+          exercises:10
+        },
+        {
+          title:"Using props to pass data",
+          exercises:7
+        },
+        {
+          title:"State of a component",
+          exercises:14
+        }
+      ]
+    },
+    total:{
+      title:"Number of excercises",
+    }
+  }
+
   const Header = (props) => {
     return (
       <div>
-        <h1>{props.course}</h1>
+        <h1>{props.course.course_name}</h1>
       </div>
     )
   }
   const Content =(props) =>{
+    let elements_react = []; 
+    props.elements.elements.forEach(parte => {
+      elements_react.push( React.createElement(
+        'p', null, `${parte.title} ${parte.exercises}`));
+    });
+
     return (
       <div>
-        <p>{props.part1} {props.exercises1}</p>
-        <p>{props.part2} {props.exercises2}</p>
-        <p>{props.part3} {props.exercises3}</p>
+        {elements_react}
       </div>
     )
   }
 
   const Total = (props) => {
+    let operation = 0
+    props.elements.elements.forEach(parte => {
+      operation += parte.exercises
+    });
+
     return (
       <div>
         <p>
-          Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+          {props.eval.title} = {operation}
         </p>
       </div>
     )
   }
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total  exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Header course={elements.header} />
+      <Content elements={elements.parts} />
+      <Total eval={elements.total} elements={elements.parts} />
     </div>
   )
 }
